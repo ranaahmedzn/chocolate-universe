81de97e472f16a25e547cd35fd28a0fa2c2918e5
Rana-Sheikh <ranasheikhsbd@gmail.com>
2023-05-16 16:56:23 +0600

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-center'>
+        <h1 className='font-bold text-5xl my-3'>Welcome to Chocolate Universe!!</h1>
+        <button className=' btn btn-error'>Click me</button>
+      </div>
+    </>
+      )
+}
+
+      export default App
